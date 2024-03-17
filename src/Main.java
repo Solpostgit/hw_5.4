3518edd638eb1351ced19caac5ae6ae13684a6a3
Solpostgit <begemot_off@mail.ru>
2024-03-17 11:31:59 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String[] arrBooks = {"Название1", "Имя Отчество Фамилия1", "Название2", "Имя Отчество Фамилия2","Название3", "Имя Отчество Фамилия3"};
+        for (int i = 0; i < arrBooks.length; i += 2) {
+            System.out.print("\"" + arrBooks[i] + "\"" + " " + arrBooks[i + 1]);
+            System.out.println();
+        }
+    }
+}
